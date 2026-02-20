37a9b09ae447fe93d0087661990d657adad6cbef
Yy101488 <yili25@s.sfusd.edu>
2026-02-19 17:53:57 -0800

    Added menu for backyard too

--- pause_menu.gd
+++ pause_menu.gd
@@ -20,9 +20,8 @@ func _on_button_pressed() -> void:
 
 
 func _on_button_2_pressed() -> void:
-	is_paused = false 
-	get_tree().reload_current_scene()
-
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://prototype.tscn")
 
 func _on_button_3_pressed() -> void:
 	is_paused = false
