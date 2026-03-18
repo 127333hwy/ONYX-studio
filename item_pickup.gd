3bc2a4d9aa8e2ceb3a00f4cdec81cb41ed5e7332
Yy101488 <yili25@s.sfusd.edu>
2026-03-18 11:28:45 -0700

    import the kitchen assest

--- item_pickup.gd
+++ item_pickup.gd
@@ -3,27 +3,32 @@ class_name item_pickup
 
 @export var item_scene : PackedScene
 
-@onready var prompt = $Interaction
+@onready var prompt = %Interaction
 
 var player_in_range := false
+var player_ref : CharacterBody2D = null
 
 func _ready():
 	$Area2D.body_entered.connect(_on_body_entered)
 	$Area2D.body_exited.connect(_on_body_exited)
-
+	prompt.visible = false
+	
 func _process(_delta):
 	if player_in_range and Input.is_action_just_pressed("interact"):
-		pick_up()
+		if player_ref and not player_ref.holding_item:
+			pick_up()
 
 func _on_body_entered(body):
 	if body is CharacterBody2D and body.is_in_group("player"):
 		player_in_range = true
+		player_ref = body
 		if not body.holding_item: 
 			prompt.visible = true
 
 func _on_body_exited(body):
 	if body is CharacterBody2D and body.is_in_group("player"):
 		player_in_range = false
+		player_ref = null
 		prompt.visible = false
 
 func pick_up():
@@ -31,14 +36,16 @@ func pick_up():
 	if pickup_point == null:
 		return
 	var player = get_tree().get_first_node_in_group("player")
-	if player.holding_item:
+	if player_ref.holding_item:
 		return
 		
 	var new_item = item_scene.instantiate()
-	new_item.reparent(pickup_point)
+	pickup_point.add_child(new_item)
+	
 	new_item.position = Vector2.ZERO
 
-	player.holding_item = true
-	player.held_item = new_item
+	player_ref.holding_item = true
+	player_ref.held_item = new_item
+	
 
 	prompt.visible = false
